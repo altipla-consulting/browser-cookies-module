c10769e8e0334af9572dad78e245c2c668c56fec
Ernesto Alejo <ernestokarim@gmail.com>
2020-07-05 20:15:03 +0200

    Copy code from third party library.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+
 # browser-cookies-module
+
 NPM package browser-cookies re-exposed as a ES6 module for vite.
---
+++ index.js
@@ -0,0 +1,94 @@
+
+export function set(name, value, options) {
+  // Retrieve options
+  var opts = options || {};
+
+  // Apply default value for unspecified options
+  var expires  = opts.expires;
+  var domain   = opts.domain;
+  var path     = opts.path     !== undefined ? opts.path     : '/';
+  var secure   = opts.secure;
+  var httponly = opts.httponly;
+  var samesite = opts.samesite;
+
+  // Determine cookie expiration date
+  // If succesful the result will be a valid Date, otherwise it will be an invalid Date or false(ish)
+  var expDate = expires ? new Date(
+      // in case expires is an integer, it should specify the number of days till the cookie expires
+      typeof expires === 'number' ? new Date().getTime() + (expires * 864e5) :
+      // else expires should be either a Date object or in a format recognized by Date.parse()
+      expires
+  ) : 0;
+
+  // Set cookie
+  document.cookie = name.replace(/[^+#$&^`|]/g, encodeURIComponent)                // Encode cookie name
+  .replace('(', '%28')
+  .replace(')', '%29') +
+  '=' + value.replace(/[^+#$&/:<-\[\]-}]/g, encodeURIComponent) +                  // Encode cookie value (RFC6265)
+  (expDate && expDate.getTime() >= 0 ? ';expires=' + expDate.toUTCString() : '') + // Add expiration date
+  (domain   ? ';domain=' + domain     : '') +                                      // Add domain
+  (path     ? ';path='   + path       : '') +                                      // Add path
+  (secure   ? ';secure'               : '') +                                      // Add secure option
+  (httponly ? ';httponly'             : '') +                                      // Add httponly option
+  (samesite ? ';samesite=' + samesite : '');                                       // Add samesite option
+}
+
+
+export function get(name) {
+  var cookies = document.cookie.split(';');
+  
+  // Iterate all cookies
+  while(cookies.length) {
+    var cookie = cookies.pop();
+
+    // Determine separator index ("name=value")
+    var separatorIndex = cookie.indexOf('=');
+
+    // IE<11 emits the equal sign when the cookie value is empty
+    separatorIndex = separatorIndex < 0 ? cookie.length : separatorIndex;
+
+    var cookie_name = decodeURIComponent(cookie.slice(0, separatorIndex).replace(/^\s+/, ''));
+
+    // Return cookie value if the name matches
+    if (cookie_name === name) {
+      return decodeURIComponent(cookie.slice(separatorIndex + 1));
+    }
+  }
+
+  // Return `null` as the cookie was not found
+  return null;
+}
+
+
+export function erase(name, options) {
+  set(name, '', {
+    expires:  -1,
+    domain:   options && options.domain,
+    path:     options && options.path,
+    secure:   0,
+    httponly: 0}
+  );
+}
+
+
+export function all() {
+  var all = {};
+  var cookies = document.cookie.split(';');
+
+  // Iterate all cookies
+  while(cookies.length) {
+    var cookie = cookies.pop();
+
+    // Determine separator index ("name=value")
+    var separatorIndex = cookie.indexOf('=');
+
+    // IE<11 emits the equal sign when the cookie value is empty
+    separatorIndex = separatorIndex < 0 ? cookie.length : separatorIndex;
+
+    // add the cookie name and value to the `all` object
+    var cookie_name = decodeURIComponent(cookie.slice(0, separatorIndex).replace(/^\s+/, ''));
+    all[cookie_name] = decodeURIComponent(cookie.slice(separatorIndex + 1));
+  }
+
+  return all;
+};
